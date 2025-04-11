14dde23b29e4a5dd938576e385a5599531f99705
trancollin <trancollin2002@gmail.com>
2025-04-11 05:12:37 -0500

    Finished laying out interest form, about me, and text content of services sections.
    Nelson picture is currently commented out for performance.
    
    To do:
    -Apply respective links to instagram logo and interest form button
    -Correct alignment on lists in "pricing"
    -Add section below "services" containing pictures and descriptions.

---
+++ script.js
@@ -0,0 +1,20 @@
+let read_buttons = document.querySelectorAll(".read");
+
+for(let i = 0; i < read_buttons.length; i++){
+    let button = read_buttons[i];
+    button.addEventListener("click", () => {
+        let add_text = button.previousElementSibling;
+        let dots = add_text.previousElementSibling.lastChild;
+
+        if(dots.style.display === "none"){
+            dots.style.display = "inline";
+            button.textContent = "Read More";
+            add_text.style.display = "none";
+        }
+        else{
+            dots.style.display = "none";
+            button.textContent = "Read Less";
+            add_text.style.display = "inline";
+        }
+    });
+}
